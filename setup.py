32302e625ccd609bcd899fef5dbf29661701ad89
omkar <omkargwagholikar@gmail.com>
2025-09-15 17:59:13 +0000

    Updated description in pyproject.toml

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ import setuptools
 from brags.version import Version
 
 
-setuptools.setup(name='adora',
+setuptools.setup(name='brags',
                  version=Version('1.0.0').number,
                  description='Python Package Boilerplate',
                  long_description=open('README.md').read().strip(),
                  author='Package Author',
-                 author_email='you@youremail.com',
+                 author_email='omkargwagholikar@gmail.com',
                  url='http://path-to-my-packagename',
                  py_modules=['packagename'],
                  install_requires=[],
                  license='MIT License',
                  zip_safe=False,
-                 keywords='boilerplate package',
+                 keywords='rag go filewatcher',
                  classifiers=['Packages', 'Boilerplate'])
